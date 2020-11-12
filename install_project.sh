1f76b1236434a100af9f955a9a3ab17c14d85d90
Destriery <izzon@yandex.ru>
2020-11-12 17:00:26 +0300

    fix: more install_project

--- install_project.sh
+++ install_project.sh
@@ -5,41 +5,63 @@
 # Настраиваем pre-commit
 
 VENV_NAME=".venv"
+VENV_DEV_NAME=".venv-dev"
 
-INSTALL=\
-`echo "Install virtualenv in $VENV_NAME"
-python3 -m venv $VENV_NAME
-$VENV_NAME/bin/pip install -r requirements.txt
-
-
-VENV_DEV_NAME=".venv-dev"`
-
-INSTALL_FOR_DEV=\
-`echo "Install dev virtualenv in $VENV_DEV_NAME"
-python3 -m venv $VENV_DEV_NAME
-$VENV_DEV_NAME/bin/pip install pytest flake8 pre-commit
-$VENV_DEV_NAME/bin/pip install -r requirements.txt
-$VENV_DEV_NAME/bin/pip install -r requirements-dev.txt
-
-echo "Install .pre-commit"
-pre-commit install
-
-echo "Check project"
-pre-commit run --all-files`
-
-
-
+echo ""
 MENU=(
     "Install Project"
     "Install Project for development"
 )
 select menu in "${MENU[@]}" ; do
     case $REPLY in
-        1) echo $INSTALL;;
-        2) echo $INSTALL_FOR_DEV;;
+        1)
+            break
+            ;;
+        2)
+            echo "Install dev virtualenv in $VENV_DEV_NAME"
+            python3 -m venv $VENV_DEV_NAME
+            $VENV_DEV_NAME/bin/pip install pytest flake8 pre-commit
+            $VENV_DEV_NAME/bin/pip install -r requirements.txt
+            $VENV_DEV_NAME/bin/pip install -r requirements-dev.txt
+
+            echo "Install pre-commit"
+            $VENV_DEV_NAME/bin/pre-commit install
+
+            echo "Check project"
+            $VENV_DEV_NAME/bin/pre-commit run --all-files
+
+            break
+            ;;
     esac
 done
 
-echo "Comleted!"
+# В любом случае устанавливаем основное окружение
 
-echo "You may start project by uvicorn: uvicorn main:app --reload"
+echo ""
+echo "Install virtualenv in $VENV_NAME"
+echo ""
+python3 -m venv $VENV_NAME
+$VENV_NAME/bin/pip install -r requirements.txt
+
+echo ""
+echo "Comleted!"
+echo ""
+echo "Start project?"
+
+MENU=(
+    "Yes"
+    "No"
+)
+select menu in "${MENU[@]}" ; do
+    case $REPLY in
+        1)
+            .venv/bin/uvicorn main:app --reload
+            break
+            ;;
+        2)
+            echo ""
+            echo "You may start project by uvicorn: .venv/bin/uvicorn main:app --reload"
+            break
+            ;;
+    esac
+done
